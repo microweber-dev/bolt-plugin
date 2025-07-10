2ef7f579ac6e01ba6ed8b45440efd70aa42e838a
Bozhidar <bobi@microweber.com>
2025-07-10 22:15:04 +0300

    up

--- app/mw-plugin/app/Providers/Filament/AdminPanelProvider.php
+++ app/mw-plugin/app/Providers/Filament/AdminPanelProvider.php
@@ -34,6 +34,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->topNavigation(true)
             ->brandLogo(url('logo.png'))
+            ->brandName('Microweber Toolkit')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
