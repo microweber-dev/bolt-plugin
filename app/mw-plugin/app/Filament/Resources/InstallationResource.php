66bfd5ede3555f400047ec00fbd616b629662a3f
Bozhidar <bobi@microweber.com>
2025-07-10 17:25:44 +0300

    up

---
+++ app/mw-plugin/app/Filament/Resources/InstallationResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\InstallationResource\Pages;
+use App\Filament\Resources\InstallationResource\RelationManagers;
+use App\Models\Installation;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class InstallationResource extends Resource
+{
+    protected static ?string $model = Installation::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListInstallations::route('/'),
+            'create' => Pages\CreateInstallation::route('/create'),
+            'edit' => Pages\EditInstallation::route('/{record}/edit'),
+        ];
+    }
+}
--- app/mw-plugin/app/Models/Installation.php
+++ app/mw-plugin/app/Models/Installation.php
@@ -6,14 +6,14 @@ use App\Models\Domain;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class MicroweberInstallation extends Model
+class Installation extends Model
 {
     use HasFactory;
 
-    public function domain()
-    {
-        return $this->hasOne(Domain::class, 'id', 'domain_id');
-    }
+//    public function domain()
+//    {
+//        return $this->hasOne(Domain::class, 'id', 'domain_id');
+//    }
 
     public function getTemplateAttribute()
     {
